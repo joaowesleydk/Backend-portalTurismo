153b43ebf2c7bffd194b6e57e8d6b42701cf3376
joaowesleydk <joaodkind@gmail.com>
2025-07-14 17:33:43 -0300

    CREATE POST USER ROUTER

---
+++ server.js
@@ -0,0 +1,27 @@
+// Importa a instância do Sequelize configurada e o modelo de usuário
+const sequelize = require('./config/db');
+require('dotenv').config();
+const express = require('express')
+ 
+const userRoutes = require('./routes/userRoutes')
+ 
+const app = express();
+ 
+app.use(express.json());
+ 
+app.get('/', (req, res)=> res.send('api funcionando'))
+ 
+app.use('/api/users', userRoutes)
+ 
+const PORT = process.env.PORT;
+ 
+sequelize.authenticate()
+  .then(() => {
+    console.log('servidor online e conectado com o DB')
+    return sequelize.sync();
+  })
+  .then(() =>{
+    console.log('banco de dados sincronizado')
+    app.listen(PORT, () => console.log("SERVIDOR RODANDO NA PORTA: " + PORT))
+  }).catch(erro => console.log("Erro interno do servidor", erro))
+ 
